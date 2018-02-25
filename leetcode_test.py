828ee9bee6aa6a2866823ba2d1b914eec04ff9f9
Enthuisasticpessimist <36471701+Enthuisasticpessimist@users.noreply.github.com>
2018-02-25 15:54:50 +0800

    isPalindrome1.2
    just solve by 1 line

--- leetcode_test.py
+++ leetcode_test.py
@@ -1,23 +1,27 @@
 def isPalindrome(x):
-    ##判断输入值是否小于0，如果小于0，则返回False（不是回文）
-    if x < 0:
-        return False
+#######################isPalindrome1.2
+    return str(x)[::-1] == str(x) ##将x转换成字符串并倒置，并与x只转换成字符串的结果进行比较，相同则是回文，不相同则不是回文
 
-    
-    r = 1  ##创建临时变量，作为运算判断的辅助
-    ##将r设置与输入值同位的除数（输入为123，则r=100）
-    while x / r >= 10:
-        r *= 10
-
-    while r > 1:
-        left, x =divmod(x, r) ##取得x的第一位数字给left，再去除x的第一位数字
-        x, right = divmod(x, 10) ##取得x的最后一位数字给right，再去除x的最后一位数字
-        if left != right:
-            return False ##头尾相对应的数字不同则返回错误
-        r //= 100 ##由于x缩小两位，临时输出也缩小两位
-
-    return True ##头尾相对应的数字全部相同，返回True
-
+#######################isPalindrome1.1
+####    ##判断输入值是否小于0，如果小于0，则返回False（不是回文）
+####    if x < 0:
+####        return False
+####
+####    
+####    r = 1  ##创建临时变量，作为运算判断的辅助
+####    ##将r设置与输入值同位的除数（输入为123，则r=100）
+####    while x / r >= 10:
+####        r *= 10
+####
+####    while r > 1:
+####        left, x =divmod(x, r) ##取得x的第一位数字给left，再去除x的第一位数字
+####        x, right = divmod(x, 10) ##取得x的最后一位数字给right，再去除x的最后一位数字
+####        if left != right:
+####            return False ##头尾相对应的数字不同则返回错误
+####        r //= 100 ##由于x缩小两位，临时输出也缩小两位
+####
+####    return True ##头尾相对应的数字全部相同，返回True
+#######################
 ##测试方法：
 isPalindrome(11)
 isPalindrome(-11)
